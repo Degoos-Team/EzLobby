0b176a444bba09d5cf3b3afac5a35651b199af78
Gael Rial Costas <gael.rial.costas@gmail.com>
2026-01-18 14:46:30 +0100

    feat: Added block event systems for handling permissions on block interactions (Break, Place, Damage, Use).

--- src/main/kotlin/com/degoos/hytale/ezlobby/EzLobby.kt
+++ src/main/kotlin/com/degoos/hytale/ezlobby/EzLobby.kt
@@ -2,24 +2,35 @@ package com.degoos.hytale.ezlobby
 
 import com.degoos.hytale.ezlobby.commands.NotifyCommand
 import com.degoos.hytale.ezlobby.commands.ezlobby.EzLobbyCommand
+import com.degoos.hytale.ezlobby.systems.BreakEventSystem
+import com.degoos.hytale.ezlobby.systems.DamageEventSystem
+import com.degoos.hytale.ezlobby.systems.PlaceEventSystem
+import com.degoos.hytale.ezlobby.systems.UseEventSystem
 import com.degoos.kayle.KotlinPlugin
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit
-import java.util.concurrent.CompletableFuture
 
 @Suppress("unused")
 class EzLobby(init: JavaPluginInit) : KotlinPlugin(init) {
+
+    companion object {
+    }
 
     override fun setup() {
         logger.atInfo().log("[Degoos:EzLobby] Plugin has been loaded")
     }
 
     override fun start() {
-        CompletableFuture.runAsync {
-            commandRegistry.registerCommand(EzLobbyCommand())
-            logger.atConfig().log("[Degoos:EzLobby] EzLobby Command Registered")
-            commandRegistry.registerCommand(NotifyCommand())
-            logger.atConfig().log("[Degoos:EzLobby] EzNotify Command Registered")
-        }
+        entityStoreRegistry.registerSystem(BreakEventSystem())
+        entityStoreRegistry.registerSystem(PlaceEventSystem())
+        entityStoreRegistry.registerSystem(DamageEventSystem())
+        entityStoreRegistry.registerSystem(UseEventSystem())
+
+        commandRegistry.registerCommand(EzLobbyCommand())
+        logger.atConfig().log("[Degoos:EzLobby] EzLobby Command Registered")
+        commandRegistry.registerCommand(NotifyCommand())
+        logger.atConfig().log("[Degoos:EzLobby] EzNotify Command Registered")
+
+
     }
 
     override fun shutdown() {
