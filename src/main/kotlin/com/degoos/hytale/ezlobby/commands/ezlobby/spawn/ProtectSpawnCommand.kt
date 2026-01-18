70f7325fb258434f282a84b4225dd6ff5755c424
Gael Rial Costas <gael.rial.costas@gmail.com>
2026-01-18 20:25:10 +0100

    perf: improve command 'ezlobby spawn protect'.

--- src/main/kotlin/com/degoos/hytale/ezlobby/commands/ezlobby/spawn/ProtectSpawnCommand.kt
+++ src/main/kotlin/com/degoos/hytale/ezlobby/commands/ezlobby/spawn/ProtectSpawnCommand.kt
@@ -1,41 +1,39 @@
 package com.degoos.hytale.ezlobby.commands.ezlobby.spawn
 
 import com.degoos.hytale.ezlobby.EzLobby
-import com.hypixel.hytale.component.Ref
-import com.hypixel.hytale.component.Store
+import com.degoos.kayle.KotlinPlugin
+import com.degoos.kayle.dsl.dispatcher
 import com.hypixel.hytale.protocol.GameMode
 import com.hypixel.hytale.server.core.Message
+import com.hypixel.hytale.server.core.command.system.AbstractCommand
 import com.hypixel.hytale.server.core.command.system.CommandContext
-import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand
-import com.hypixel.hytale.server.core.universe.PlayerRef
 import com.hypixel.hytale.server.core.universe.Universe
-import com.hypixel.hytale.server.core.universe.world.World
-import com.hypixel.hytale.server.core.universe.world.WorldConfig
-import com.hypixel.hytale.server.core.universe.world.storage.EntityStore
+import kotlinx.coroutines.future.asCompletableFuture
+import kotlinx.coroutines.launch
+import java.util.concurrent.CompletableFuture
 
-class ProtectSpawnCommand : AbstractPlayerCommand("protect", "ezlobby.commands.ezlobby.spawn.protect.desc") {
-    override fun execute(
-        context: CommandContext,
-        store: Store<EntityStore?>,
-        refStore: Ref<EntityStore?>,
-        playerRef: PlayerRef,
-        world: World
-    ) {
+class ProtectSpawnCommand(val plugin: KotlinPlugin) :
+    AbstractCommand("protect", "ezlobby.commands.ezlobby.spawn.protect.desc") {
+
+    override fun execute(context: CommandContext): CompletableFuture<Void>? {
         val worldName = EzLobby.getMainConfig()?.get()?.spawnPointWorldName
-        val spawnWorld = if(worldName != null) Universe.get().getWorld(worldName) else null
+        val spawnWorld = if (worldName != null) Universe.get().getWorld(worldName) else null
 
         if (spawnWorld == null) {
             context.sendMessage(Message.raw("[EzLobby] Could not set spawn point, World(name=${worldName}) does not exist :/"))
-            return
+            return null
         }
 
+        val job = plugin.launch(spawnWorld.dispatcher) {
+            spawnWorld.worldConfig.isSpawningNPC = false
+            spawnWorld.worldConfig.gameMode = GameMode.Adventure
+            spawnWorld.worldConfig.isPvpEnabled = false
 
-        spawnWorld.worldConfig.isSpawningNPC = false
-        spawnWorld.worldConfig.gameMode = GameMode.Adventure
-        spawnWorld.worldConfig.isPvpEnabled = false
-
-        Universe.get().worldConfigProvider.save(spawnWorld.savePath, spawnWorld.worldConfig, spawnWorld).thenRun {
-            playerRef.sendMessage(Message.raw("[EzLobby] Spawn protection enabled for World(name=${worldName})."))
+            Universe.get().worldConfigProvider.save(spawnWorld.savePath, spawnWorld.worldConfig, spawnWorld).thenRun {
+                context.sendMessage(Message.raw("[EzLobby] Spawn protection enabled for World(name=${worldName})."))
+            }
         }
+
+        return job.asCompletableFuture().thenApply { return@thenApply null }
     }
 }
